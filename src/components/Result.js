8de1f15bb086a3e9b8cbf414a667a8692269a4d7
Mateusz Miloszewski <monitorwebdesign@gmail.com>
2020-01-20 16:47:08 +0100

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,11 +6,17 @@ const Result  = props => {
     let content = null;
 
     if(!err && city){
+        const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString()
+        const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
         content = (
             <div>
                 <h3>Searching results for <strong>{city}</strong></h3>
                 <h4>Day and time: {date}</h4>
-                <h4>Current weather {temp}</h4>
+                <h4>Current weather: {temp}</h4>
+                <h4>Sunrise: {sunriseTime}</h4>
+                <h4>Sunset: {sunsetTime}</h4>
+                <h4>Pressure: {pressure}</h4>
+                <h4>Wind: {wind}</h4>
             </div>
         )
     }
